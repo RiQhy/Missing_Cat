4ffa4461fec23669d58aee146a0171cbaaf44d3c
RiQhy <riku.nokelainen@metropolia.fi>
2023-04-19 10:40:42 +0300

    package.json files

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+/*const cors = require('cors');
+const catRoute = require('./routes/catRoute');
+const userRoute = require('./routes/userRoute');
+const authRoute = require('./routes/authRoute');
+const passport = require('./utils/passport');*/
+const app = express();
+const port = 3000;
+
+app.use(express.static('ui'));
+app.use('/img', express.static('img'));
+//Add cors headers
+//app.use(cors());
+
+app.use(express.json());
+/*app.use(express.urlencoded({extended: true}));
+app.use(passport.initialize());
+
+app.use('/auth', authRoute);
+app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
+app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);*/
+
+
+app.listen(port, () => console.log(`Website listening on port ${port}`));
